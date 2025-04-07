6214414a978841a8d3765aa438108258bfb64917
EnnioCristianelli <10846158@polimi.it>
2025-04-08 00:07:50 +0200

    Client Controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -26,4 +26,6 @@ module it.polimi.ingsw.galaxytruckerproject {
     exports it.polimi.ingsw.galaxytruckerproject.model.goods;
     exports it.polimi.ingsw.galaxytruckerproject.network.SOCKET.message;
     opens it.polimi.ingsw.galaxytruckerproject.model.goods to com.fasterxml.jackson.databind;
+    exports it.polimi.ingsw.galaxytruckerproject.controller.client;
+    opens it.polimi.ingsw.galaxytruckerproject.controller.client to com.fasterxml.jackson.databind;
 }
