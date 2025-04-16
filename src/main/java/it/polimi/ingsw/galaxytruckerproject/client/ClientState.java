0a9759344c436ba98c76be25fc944185dcc827bb
EnnioCristianelli <10846158@polimi.it>
2025-04-16 11:18:50 +0200

    Client states description

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/client/ClientState.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/client/ClientState.java
@@ -3,17 +3,17 @@ package it.polimi.ingsw.galaxytruckerproject.client;
 public enum ClientState {
     CHOOSE_UI,
     CHOOSE_CONNECTION_TYPE,
-    LOBBY,
-    LOGIN,
+    LOBBY,//choose name
+    LOGIN,//choose game
     COLOR_CHOICE,
-    ACTION,
-    COORD_REQUEST,
+    ACTION,// yes or no
+    COORD_REQUEST,//usare askCoordinates in modo da settare per la richiesta di coordinata corretta
     MANAGE_GOODS,
-    PLANET_CHOICE,
-    S_END_DRAW_TILE_CARD,
-    MANAGE_CARDS,
-    S_MANAGE_DRAWN_TILE,
-    S_FINISHED,
+    PLANET_CHOICE,//scelta su quale pianteta atterrare
+    S_END_DRAW_TILE_CARD,//può scegliere se finire la costruzione o pescale una tle/carta
+    MANAGE_CARDS,//scorre le carte che ha in mano durante la fase di costruzione
+    S_MANAGE_DRAWN_TILE,//ruota, posiziona o scarta
+    S_FINISHED,//costruzione della shipboard finita, attesa della verifica correttezza
     WAIT_OTHER_PLAYER_ACTION,
     START_SHIP_CREATION
 }
