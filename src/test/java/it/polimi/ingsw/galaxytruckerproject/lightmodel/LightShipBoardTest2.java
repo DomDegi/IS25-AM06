4e1a3411c4659a6076b016b0f97ef3ad1bd9b417
soheil akharraze <soheil.akharraze@mail.polimi.it>
2025-05-02 11:55:28 +0200

    testing

--- src/test/java/it/polimi/ingsw/galaxytruckerproject/lightmodel/LightShipBoardTest2.java
+++ src/test/java/it/polimi/ingsw/galaxytruckerproject/lightmodel/LightShipBoardTest2.java
@@ -18,6 +18,8 @@ class LightShipBoardTest2 {
         LightPlayer lightPlayer = new LightPlayer("Per favore", PlayersColor.GREEN);
         LightShipBoard lightShipBoard = new LightShipBoard(lightPlayer);
         lightShipBoard.initializeLevel2();
+        boolean cazzo = lightPlayer.getShipBoard().equals(lightShipBoard);
+        assertTrue(cazzo);
     }
 
     @Test
