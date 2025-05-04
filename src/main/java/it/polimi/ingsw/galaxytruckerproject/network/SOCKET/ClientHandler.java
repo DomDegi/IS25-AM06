243076351cc00d802b931bf33effd4d614a90d38
soheil akharraze <soheil.akharraze@mail.polimi.it>
2025-05-04 23:00:35 +0200

    client Handler

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/network/SOCKET/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/network/SOCKET/ClientHandler.java
@@ -1,33 +1,64 @@
 package it.polimi.ingsw.galaxytruckerproject.network.SOCKET;
 
+import it.polimi.ingsw.galaxytruckerproject.controller.interfaces.ControllerInterface;
+import it.polimi.ingsw.galaxytruckerproject.network.RMI.Client.VirtualViewRMI;
+import it.polimi.ingsw.galaxytruckerproject.network.VirtualView;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
 
-    private Socket socket;
-    private ObjectInputStream input;
-    //private VirtualView virtualView;
+    //socket of the client
+    private final Socket clientSocket;
 
-    public ClientHandler(Socket socket) {
-        this.socket = socket;
-        try {
-            this.input = new ObjectInputStream(socket.getInputStream());
+    //VirtualView (non se va messa proprio la virtualViewSocket nel caso)
+    private VirtualView owner;
+
+    //output stream to the client
+    private ObjectOutputStream output;
+
+    //input stream from the client
+    private ObjectInputStream input;
+
+    //controller interface
+    private ControllerInterface controller;
+
+    //set to true only when run ends
+    private boolean readyToGo = false;
+
+    //Initializes a new handler using a specific socket (connected to the client)
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        //Non mi è ancora tutto chiarissimo ma mettere la new input stream qui
+        //implicherebbe che in caso di problemi non riuscimo manco ad inizializzare
+        // il client handler. Ha più senso metterlo in RUN
+        /*try {
+            this.input = new ObjectInputStream(clientSocket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
     public void run() {
         try {
-            while (true) {
-                Object command = input.readObject();
-                //analizza command per inviarlo al controller
+            output = new ObjectOutputStream(clientSocket.getOutputStream());
+            input = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Could not open connection to " + clientSocket.getInetAddress());
+            return;
+        }
+        readyToGo = true;
+        //once the socket-connection is ready, we wait for the VirtualView and the Controller
+        while(this.owner == null && this.controller==null){
+            try{
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            System.out.println("Connessione persa con il client.");
         }
     }
 }
