abc3b2349764b08928eb6e7fa99a2139a1e39044
fedebulfari <federicoernesto.bulfari@mail.polimi.it>
2025-05-13 14:28:33 +0200

    solved problems

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/client/TextualInputParser.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/client/TextualInputParser.java
@@ -211,6 +211,10 @@ public class TextualInputParser {
                                     }
                                 }
                             }
+                            default -> {
+                                view.wrongLocalInput();
+                                return false;
+                            }
                         }
                     }
                     default -> {
