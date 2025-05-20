7c14573ac1517e6e54f341af5ba5a09a27ddf51c
DomDegi <10854350@polimi.it>
2025-05-20 20:23:36 +0200

    Update Smugglers.java

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/model/cards/Smugglers.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/model/cards/Smugglers.java
@@ -241,4 +241,12 @@ public class Smugglers extends Enemies{
         }
         return string.toString();
     }
+
+    @Override
+    public void playerDisconnected(String playerName) {
+        if (currentPlayer != null && currentPlayer.getPlayerName().equals(playerName)) {
+            playerIndex--;
+            nextPlayer();
+        }
+    }
 }
