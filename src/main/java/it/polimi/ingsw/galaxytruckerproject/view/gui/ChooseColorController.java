66121d4e4bb75e00c0bd1a903ea8d14ce85e1796
fedebulfari <federicoernesto.bulfari@mail.polimi.it>
2025-05-21 11:09:39 +0200

    corrected merge

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/view/gui/ChooseColorController.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/view/gui/ChooseColorController.java
@@ -13,28 +13,28 @@ public class ChooseColorController {
     public void red() throws IOException {
         if(GUI.getController().getState() == ClientState.COLOR_CHOICE0){
             GUI.createGame();
-        }else
-            GUI.setColor(PlayersColor.RED);
+        }
+        GUI.setColor(PlayersColor.RED);
     }
     @FXML
     public void yellow() throws IOException {
         if(GUI.getController().getState() == ClientState.COLOR_CHOICE0){
             GUI.createGame();
-        }else
-            GUI.setColor(PlayersColor.YELLOW);
+        }
+        GUI.setColor(PlayersColor.YELLOW);
     }
     @FXML
     public void green() throws IOException {
         if(GUI.getController().getState() == ClientState.COLOR_CHOICE0){
             GUI.createGame();
-        }else
-            GUI.setColor(PlayersColor.GREEN);
+        }
+        GUI.setColor(PlayersColor.GREEN);
     }
     @FXML
     public void blue() throws IOException {
         if(GUI.getController().getState() == ClientState.COLOR_CHOICE0){
             GUI.createGame();
-        }else
-            GUI.setColor(PlayersColor.BLUE);
+        }
+        GUI.setColor(PlayersColor.BLUE);
     }
 }
