6ddaf869528eba3256ad273052dbd0d83f10704f
DomDegi <10854350@polimi.it>
2025-06-25 12:03:30 +0200

    changed timeout client side

--- src/main/java/it/polimi/ingsw/galaxytruckerproject/client/ClientController.java
+++ src/main/java/it/polimi/ingsw/galaxytruckerproject/client/ClientController.java
@@ -1809,7 +1809,7 @@ public class ClientController {
 
         serverWatchdogThread = new Thread(() -> {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(5000);
                 setOffline();
             } catch (InterruptedException ignored) {
             }
